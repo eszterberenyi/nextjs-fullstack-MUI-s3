d63da01c1a4fcd2ee086f9a2ad8d9b2db2f07fe6
eszterberenyi <e.eszterberenyi@gmail.com>
2023-10-01 13:48:15 +0200

    delete endpoint

--- src/app/api/contacts/route.ts
+++
@@ -1,7 +0,0 @@
-import {Request, Response} from "next/dist/compiled/@edge-runtime/primitives";
-import { generateUploadURL } from '../../../s3'
-
-export async function POST(req: Request, res: Response) {
-    const uploadURL = await generateUploadURL();
-
-}
