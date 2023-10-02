c6433e4684de7f531bb9f9b855f400c1bd2290c2
eszterberenyi <e.eszterberenyi@gmail.com>
2023-10-02 15:03:04 +0200

    create contact with styles

--- src/components/contacts/Contact.tsx
+++ src/components/contacts/Contact.tsx
@@ -3,6 +3,7 @@ import {IconButton, Grid, Avatar} from "@mui/material";
 import React from 'react'
 import { ContactWithId } from "@/prisma/seed";
 import { awsUrl } from "../../s3";
+import styles from './Contact.module.css'
 
 interface Props {
     contactData: ContactWithId
@@ -10,20 +11,68 @@ interface Props {
 
 const Contact = (props: Props) => {
     return (
-        <Grid container key={props.contactData.id}>
+        <Grid
+            container
+            key={props.contactData.id}
+            sx={{ columnGap: { xs: 2, md: 0 } }}
+            flexWrap='nowrap'
+        >
+
             <Grid item xs={2}>
                 <Avatar
                     src={ props.contactData.hasPhoto ? `${awsUrl}${props.contactData.id}` : ''}
                 />
             </Grid>
-            <Grid item container direction='column' xs={6} rowGap={1}>
+
+            <Grid item container direction='column' xs={10} md={6} rowGap={1}>
                 <Grid item>
-                    <h3>{props.contactData.name}</h3>
+                    <h3 className={styles.contactName}>{props.contactData.name}</h3>
                 </Grid>
                 <Grid item>
-                    <p className="message" style={{color: 'rgba(255, 255, 255, 0.56)'}}>{props.contactData.phone}</p>
+                    <p className={`message ${styles.phoneNumber}`} style={{color: 'rgba(255, 255, 255, 0.56)'}}>{props.contactData.phone}</p>
                 </Grid>
             </Grid>
+
+            <Grid
+                item
+                container
+                justifyContent="flex-end"
+                spacing={2}
+                pr={1}
+                className={styles.hoverableIcons}
+            >
+                <Grid item>
+                    <IconButton disabled className={styles.icon} size='small'>
+                        <img src="/icons/Mute.png" alt="mute icon" className={styles.iconImg}/>
+                    </IconButton>
+                </Grid>
+                <Grid item>
+                    <IconButton disabled className={styles.icon} size="small">
+                        <img src="/icons/Call.png" alt="call icon" className={styles.iconImg}/>
+                    </IconButton>
+                </Grid>
+                <Grid item>
+                    <IconButton disabled className={styles.icon} size='small'>
+                        <img src="/icons/More.png" alt="more options icon" className={styles.iconImg}/>
+                    </IconButton>
+                </Grid>
+            </Grid>
+
+            <Grid
+                 item
+                 container
+                 justifyContent="flex-end"
+                 spacing={2}
+                 pr={1}
+                 className={styles.nonHoverableIcons}
+            >
+                <Grid item>
+                    <IconButton disabled className={styles.icon} size='small'>
+                        <img src="/icons/More.png" alt="more options icon" className={styles.iconImg}/>
+                    </IconButton>
+                </Grid>
+            </Grid>
+
         </Grid>
     )
 }
